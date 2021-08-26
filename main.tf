1625260eda87834a3b991d9338af9a3ef3894f74
phanee <phanee47@gmail.com>
2021-08-26 21:12:51 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  profile = "prod"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "Public_Subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr_blocks[0]
+
+  tags = {
+    Name = "Main"
+  }
+}
